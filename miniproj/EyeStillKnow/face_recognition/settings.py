1285995b150bee1d016599f36acf653c37fd09cf
AKSHATA-22 <akshatasangwai1234@gmail.com>
2022-11-12 20:42:51 +0530

    Assets removed and static added

--- miniproj/EyeStillKnow/face_recognition/settings.py
+++ miniproj/EyeStillKnow/face_recognition/settings.py
@@ -125,7 +125,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assests')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# ALLOWED_HOSTS = ['*']
+# X_FRAME_OPTIONS = '*'
 
